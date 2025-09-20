1f84b9d092fe8458d7a4cb4399e474802b52c43b
Jer-rz <jerexxbalendo@gmail.com>
2025-09-20 23:40:09 +0800

    chore: prepare for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+# For example, to avoid duplicate:
+products_data = [
+  { code: "GR1", name: "Green Tea", price: 3.11 },
+  { code: "SR1", name: "Strawberries", price: 5.00 },
+  { code: "CF1", name: "Coffee", price: 11.23 }
+]
+
+products_data.each do |attributes|
+  Product.find_or_create_by(code: attributes[:code]) do |product|
+    product.name = attributes[:name]
+    product.price = attributes[:price]
+  end
+end
